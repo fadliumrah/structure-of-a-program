ad2d3882b3f5ee2896b5abf91eecbdb070d6ab0e
muhamad fadli <84912914+fadliumrah@users.noreply.github.com>
2021-07-19 21:29:03 +0700

    Create structure_of_c++.cpp

---
+++ structure_of_c++.cpp
@@ -0,0 +1,11 @@
+#include<iostream>//pustaka bahasa c++ atau library
+using namespace std; //deklarasi namespace menggunakan std
+
+int main(){//fungsi utama atau fungsi body
+    //single line commetn
+    /*multi line comment
+    digunakan untuk komentar dengan banyak baris
+    dua baris atau lebih*/
+
+    return 0;//mengembalikan nilai ke nol setelah compile program
+}
