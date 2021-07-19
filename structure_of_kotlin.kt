73479b2f24dc28b93807a5689a6ba5ed3ee4858d
muhamad fadli <84912914+fadliumrah@users.noreply.github.com>
2021-07-19 22:07:16 +0700

    kotlin

--- structure_of_kotlin.kt
+++ structure_of_kotlin.kt
@@ -3,3 +3,15 @@ fun main(){
     /*multi line comment
     digunakan untuk komentar dua baris atau lebih */
 }
+
+fun sum(){
+
+}
+class kelas{
+    
+    //data dan variabel
+        public
+        fun foo() {
+            
+        }
+}
