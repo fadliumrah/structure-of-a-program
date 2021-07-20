180beddece1de3cb7bab21f3bcc25e0e7910c167
muhamad fadli <84912914+fadliumrah@users.noreply.github.com>
2021-07-20 09:15:38 +0700

    fungsi dan class

--- structure_of_kotlin.kt
+++ structure_of_kotlin.kt
@@ -1,17 +1,17 @@
-fun main(){
-    //single line
+fun main(){ //entry point atau fungsi main atau fungsi body
+    //single line comment
     /*multi line comment
     digunakan untuk komentar dua baris atau lebih */
 }
 
-fun sum(){
+fun sum(){//fungsi dengan nama "sum"
 
 }
-class kelas{
+class kelas{//object oriented programming atau class
     
     //data dan variabel
         public
-        fun foo() {
+        fun del() {//fungsi dalam kelas yang bernama "del"
             
         }
 }
