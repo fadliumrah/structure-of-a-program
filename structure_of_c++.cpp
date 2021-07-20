9b1ce7fde851f2d69587257265ce0684f78e83e9
muhamad fadli <84912914+fadliumrah@users.noreply.github.com>
2021-07-20 09:20:44 +0700

    kelas

--- structure_of_c++.cpp
+++ structure_of_c++.cpp
@@ -2,23 +2,26 @@
 using namespace std; //deklarasi namespace menggunakan std
 
 class kelas{//OOP
-    //hak akses ex public:
-    //data dan variabel: string data;
-        //hak akses fungsi ex public:
+    public://hak akses ex public:
+    string nama="fadli";//data dan variabel: string data;
+        public://hak akses fungsi ex public:
         void fungsi(){//fungsi void atau fungsi yang tidak ada nilai kembalian
-
+            cout<<nama<<endl;
         }
 };
-void fungsi()
+void fungsi()//fungsi tanpa nilai kembalian dengan nama "fungsi"
 {
 
 }
 
-int main(){//fungsi utama atau fungsi body
-    //single line commetn
+int main(){//fungsi utama atau fungsi body atau entry point
+    //single line comment
     /*multi line comment
     digunakan untuk komentar dengan banyak baris
     dua baris atau lebih*/
+
+    kelas k;//object
+    k.fungsi();
 
     return 0;//mengembalikan nilai ke nol setelah compile program
 }
